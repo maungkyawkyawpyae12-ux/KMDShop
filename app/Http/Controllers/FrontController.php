dd8b6d30ae7729c4a358bdd9f298471c65e7b48b
maungkyawkyawpyae12-uxgithup.com <maungkyawkyawpyae12@gmail.com>
2026-08-13 15:54:23 +0630

    second

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function shop()
+   {
+       return view('front.shop');
+   } 
+    public function shopItem($id)
+   {
+         return view('front.shop-item');
+   }
+}
